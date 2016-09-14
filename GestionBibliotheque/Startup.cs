7f2e24156ef83f04499f8ca891958da356c9a2cc
Gabriel Déry <gabriel.dery20@gmail.com>
2016-09-14 10:34:53 -0400

    Ajout de la structure de base du projet en ASP.net MVC

---
+++ GestionBibliotheque/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionBibliotheque.Startup))]
+namespace GestionBibliotheque
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
