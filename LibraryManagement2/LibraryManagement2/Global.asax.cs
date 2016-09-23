022f17db4a54d05d701205e92efafdc8028ab5e8
zladix <vincent.leclerc21@hotmail.fr>
2016-09-23 11:26:23 -0400

    UtilResource Fonctionne!
    Thanks Manda!

--- LibraryManagement2/LibraryManagement2/Global.asax.cs
+++ LibraryManagement2/LibraryManagement2/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using LibraryManagement2.Utils;
 
 namespace LibraryManagement2
 {
@@ -16,6 +17,7 @@ namespace LibraryManagement2
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            UtilResources.CreateInstance();
         }
     }
 }
--- LibraryManagement2/LibraryManagement2/Utility/UtilResources.cs
+++ LibraryManagement2/LibraryManagement2/Utility/UtilResources.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using LibraryManagement.Models;
+using LibraryManagement2.Models;
 using System.Data.Entity;
 
-namespace LibraryManagement.Utils
+namespace LibraryManagement2.Utils
 {
-    public class UtilResources
+    public static class UtilResources
     {
-        private LibraryManagementEntities db = new LibraryManagementEntities();
-        private int IDLanguage;
+        private static libraryManagementEntities db = new libraryManagementEntities();
+        private static int IDLanguage;
 
-        UtilResources()
+        public static void CreateInstance()
         {
             //Aller le chercher en BD et le set
             Settings config = db.Settings.Single();
             IDLanguage = config.IDLanguage;
         }
 
-        public void ChangeLanguage(string language)
+        public static void ChangeLanguage(string language)
         {
             switch (language)
             {
@@ -40,7 +40,7 @@ namespace LibraryManagement.Utils
         }
 
   
-        public string GetLabel(string label)
+        public static string GetLabel(string label)
         {
             Resources text = db.Resources.Where(c => c.TextName.Contains(label) && c.IDLanguage == IDLanguage).Single();
             if (text.Description != null && text.Description != "")
