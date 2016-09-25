867a432a555b3a7100fd247ef8ebdb822d3804c5
zladix <vincent.leclerc21@hotmail.fr>
2016-09-25 19:48:09 -0400

    Modification Langues, Combo Nouveau Compte et Correction GetLabel.

---
+++ LibraryManagement2/LibraryManagement2/Controllers/LanguageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagement2.Utils;
+
+namespace LibraryManagement2.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult setLanguage() 
+        {
+            UtilResources.ChangeLanguageTwo();
+            string url = this.Request.UrlReferrer.AbsolutePath;
+            return Redirect(url);
+        }
+    }
+}
