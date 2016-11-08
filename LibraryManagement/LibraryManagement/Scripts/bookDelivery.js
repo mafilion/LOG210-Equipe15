c9157bf845a7e671cc8edc7c6e8f0f9f1fdc8a64
Gabriel Déry <gabriel.dery20@gmail.com>
2016-11-08 15:46:15 -0500

    Partie 2 dépot du livre encore en cours...

--- LibraryManagement/LibraryManagement/Scripts/bookDelivery.js
+++ LibraryManagement/LibraryManagement/Scripts/bookDelivery.js
@@ -1,5 +1,8 @@
 ﻿
 function findLivre() {
+
+    $('#book tr:gt(0)').remove()
+
     var critere = document.getElementById('BookInfo').value;
 
     var serviceURL = '/BookDelivery/SearchBooks';
@@ -17,7 +20,7 @@ function findLivre() {
         function successFunc(data, status) {
 
             $.each(data, function (index, value) {
-                $('#book tr:last').after('<tr onClick="showBook(' + this.IDBooksCopy + ')"><td>' + this.noISBN + '</td><td>' + this.Title + '</td><td>' + this.Name + '</td><td>' + this.FirstName + ' ' + this.LastName + '</td><td>' + this.Description + '</td><td>' + this.PricePercentage + '</td></tr>');
+                $('#book tr:last').after('<tr onClick="showBook(' + this.IDBooksCopy + ')"><td>' + this.noISBN + '</td><td>' + this.Title + '</td><td>' + this.Name + '</td><td>' + this.FirstName + ' ' + this.LastName + '</td><td>' + this.Description + '</td><td>' + this.price + ' $</td></tr>');
             });
 
             $("table").addClass("show");
@@ -29,5 +32,66 @@ function findLivre() {
 }
 
 function showBook(id) {
-    alert(id);
+    
+    var serviceURL = '/BookDelivery/getBookCopy';
+
+    $.ajax({
+        type: "POST",
+        url: serviceURL,
+        data: '{"id":"' + id + '"}',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: successFunc,
+        error: errorFunc
+    });
+
+    function successFunc(data, status) {
+        $("table").removeClass("show");
+        $(".researchForm").addClass("hide");
+        $(".inputForm").addClass("show");
+        
+        $('#noISBN').val(data.noISBN);
+        $('#title').val(data.Title);
+        $('#author').val(data.Name);
+        $('#student').val(data.FirstName + " " + data.LastName);
+        $('#price').val(data.price);
+        $('#IDBookState').val(data.IDBookState);
+        $('#IDBookCopy').text(data.IDBooksCopy);
+       data
+    }
+
+    function errorFunc() {
+        alert("La recherche n'a trouvé aucun résultat correspondant.");
+    }
+}
+
+function cancel() {
+    $(".researchForm").removeClass("hide");
+    $(".inputForm").removeClass("show");
+    $("#BookInfo").val("");
+
+}
+
+function deposit() {
+
+    var BookDelivery = {}
+
+    var IDBookCopy = $("#IDBookCopy").html();
+    var IDBookState = $("#IDBookState").val();
+
+    $.ajax({
+        type: "POST",
+        url: '/BookDelivery/DepositBook',
+        contentType: "application/json; charset=utf-8",
+        data: '{"IDBooksCopy":"' + IDBookCopy + '","IDBookState":"' + IDBookState + '"}',
+        dataType: "json",
+        success: function () {
+            CleanForm();
+            alert('Le livre a bien été déposé');
+        },
+        error: function () {
+            alert('Erreur');
+        },
+    });
+
 }
