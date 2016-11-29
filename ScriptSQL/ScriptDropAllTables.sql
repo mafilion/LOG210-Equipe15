447164a60be278dc4a89d2478c8f1f40f0460540
mafilion <marcaurelef@gmail.com>
2016-11-29 18:04:46 -0500

    TestConnexionStudent et validation des mes formulaires

---
+++ ScriptSQL/ScriptDropAllTables.sql
@@ -0,0 +1,24 @@
+USE [LibraryManagement]
+GO
+
+DROP TABLE BookingLine
+Drop table Booking
+Drop table BooksCopyTransferLine
+Drop table BooksCopyTransfer
+drop table BooksCopy
+drop table BooksAuthors
+drop table Author
+drop table Book
+drop table BookState
+drop table Student
+drop table Settings
+drop table Resources
+drop table Manager
+drop table Cooperative
+
+
+
+
+GO
+
+
